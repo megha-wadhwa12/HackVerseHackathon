b4bbbfe64c0847506939c1d5d48ab0ddaaafb74a
rajashree-guha <rajashree.625.guha@gmail.com>
2024-03-29 07:51:26 +0530

    corrected error

--- Client/src/Components/Features.jsx
+++ Client/src/Components/Features.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../Context/ParentContext';
+import { useNavigate } from 'react-router-dom';
 
 const Features = () => {
+  const { login } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleTryNowClick = () => {
+    if (!login) {
+      // Redirect to login page if not logged in
+      navigate('/login'); 
+    } else {
+      console.log('Trying now...');
+    }
+  };
+
   return (
     <div className='w-full px-4 sm:px-0 h-auto text-center pt-10 sm:pt-28 mb-10 sm:mb-60 '>
       <h2 className='text-3xl sm:text-6xl font-bold text-white mb-10'>Features</h2>
@@ -12,7 +26,7 @@ const Features = () => {
             <h1 className='text-xl sm:text-3xl font-bold text-start'>Code Debugger</h1>
             <p className='text-sm sm:text-xl text-justify'>Our Code Debugger utilizes AI algorithms to swiftly detect and resolve errors in your code, offering detailed insights and suggestions to improve code quality, streamlining your debugging process effectively.</p>
             <div className='flex justify-center sm:justify-start'>
-              <button className='btn mb-4 sm:mb-0'>Try Now</button>
+              <button className='btn mb-4 sm:mb-0' onClick={handleTryNowClick}>Try Now</button>
             </div>
           </div>
         </div>
@@ -22,7 +36,7 @@ const Features = () => {
             <h1 className='text-xl sm:text-3xl font-bold text-start'>Education Topic Searcher</h1>
             <p className='text-sm sm:text-xl text-justify'>Access our AI-based Education Topic Searcher to explore diverse educational subjects. Gain comprehensive information, including subtopics and detailed descriptions, to enrich your learning experience efficiently.</p>
             <div className='flex justify-center sm:justify-start'>
-              <button className='btn mb-4 sm:mb-0'>Try Now</button>
+              <button className='btn mb-4 sm:mb-0' onClick={handleTryNowClick}>Try Now</button>
             </div>
           </div>
           <img className="w-full sm:w-1/3 rounded-box mb-5 sm:mb-0" src="https://images.hindustantimes.com/rf/image_size_640x362/HT/p2/2015/12/01/Pictures/_c34102da-9849-11e5-b4f4-1b7a09ed2cea.jpg" alt="" />
