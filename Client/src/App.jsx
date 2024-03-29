6bd9d60c1c6d33e9b0df1c2ea2cbc29af786d0d7
unknown <sp577152@gmail.com>
2024-03-29 08:21:06 +0530

    NavResponsive

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,11 +7,13 @@ import Signup from './Components/Signup'
 import Login from './Components/Login'
 import AllRoutes from './Components/AllRoutes'
 import Theme from './Components/Theme'
+import NavBar_Responsive from './Components/Navbar_Responsive/NavBar_Responsive'
 
 const App = () => {
+  const vw = window.innerWidth
   return (
     <Box overflowX={'hidden'} background={Theme.colors.primary[100]}>
-      <Navbar/>
+      {vw>426?<Navbar/>:<NavBar_Responsive/>}
       <AllRoutes />
       {/* <Home/> */}
       {/* <Content/> */}
